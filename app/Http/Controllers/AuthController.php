049af74f749ef062d057101878028d28c5ab8309
mwanginjamb <fnjambi@outlook.com>
2022-10-23 20:53:00 +0300

    Feat: API signup and auth powered by sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        return response()->json([
-            'note' => 'Hello Francois'
+        $request->validated($request->all());
+
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return $this->error('', 'Credentials are not correct.', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken,
         ]);
     }
 
@@ -21,8 +35,15 @@ class AuthController extends Controller
     public function register(StoreUserRequest $request)
     {
         $request->validated($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
         return response()->json([
-            'note' => 'You are about to signup.'
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken,
         ]);
     }
 
